539a3be2061280515a2ae20c62c89d2556ef7130
Dylan Ramelli <rameld@usi.ch>
2022-10-22 13:47:39 +0200

    Still does not work, Image.open gives a three deimentional array instead of 2, so modifying it then rotating it then modifying it back for now

--- RotateSerial/rotateSerial.py
+++ RotateSerial/rotateSerial.py
@@ -1,15 +1,17 @@
 from cmath import cos,sin
 from hmac import new
-from math import floor, pi
+from math import floor, modf, pi
 import math
-from operator import index
+from operator import index, mod
 from xml.etree.ElementTree import tostring
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np  
+from PIL import Image, ImageOps
 
 
 # As of 17 Oct, matplotlib does not work on 3.10 or above.
+
 
 
 def draw_rectangle(image, width, height):
@@ -24,7 +26,7 @@ def draw_rectangle(image, width, height):
 def rotateScatter(A,angle):
     cosine_of_angle = cos(angle)
     sin_of_angle = sin(angle)
-    newA = np.array([0]*len(A))
+    newA = np.array([0]*len(A),np.uint8)
     for i in range(0,len(A)):
         x = int(i % n)
         y = int(i / n)
@@ -36,6 +38,7 @@ def rotateScatter(A,angle):
         
         new_x = round(new_x)
         new_y = round(new_y)
+        
         
         if new_x < n and new_x >= 0 and new_y >= 0 and new_y < n:
             idx = new_y * n + new_x
@@ -66,54 +69,80 @@ def rotateGather(A,angle):
     return newA
 
 n = 50
-tuple = (2500,)
-image = np.array([0]*2500)
-draw_rectangle(image,50,50)
-            
-plt.xlim([0, n])
-plt.ylim([0, n])
+dims_tuple =(2000, 1303)
+angle = math.pi/180
+# image = np.array([0]*2500)
+# draw_rectangle(image,50,50)
 
-image.shape  = (-1,n)
+# Import grayscale image.
+image = Image.open('Roberts-Claude-Shannon-1.jpg')
 
-plt.imshow(image, cmap="gray")
-plt.savefig("original_image.png")
+# Save original image.
+image.save("original_image.png")
 
-image.shape = tuple
+#Transform image into numpy array.
+image = np.asarray(image)
+#Some info about the array
+print("Some information regarding the numpy array of the image:",
+      "\nType:" ,image.dtype, "\nShape:",image.shape, "\nStrides:",image.strides)
 
-angle = math.pi/4
+# print(image)
 
-    
+modfied_image_array = np.zeros((image.shape[0], image.shape[1]))
+for i in range(0,image.shape[0]):
+    for j in range(0,image.shape[1]):
+        modfied_image_array[i][j] = image[i][j][0]
+# image = np.reshape(image,dims_tuple)
+# print(modfied_image_array)
+modfied_image_array = modfied_image_array.flatten()
+print(modfied_image_array)
+
+
         
-rotatedimage = rotateScatter(image,angle)
-rotatedimage.shape = (-1,n)
+rotatedimage = rotateScatter(modfied_image_array,angle)
 
-plt.imshow(rotatedimage,cmap="gray")
-plt.savefig("scatterRotate_image.png")
+# print(rotatedimage)
 
 
-inverseimage = rotateGather(image,angle)
+# # print(rotatedimage)
+rotatedimage.shape = dims_tuple
 
-inverseimage.shape = (-1,n)
-plt.imshow(inverseimage,cmap="gray")
-plt.savefig("gatherRotate.png")
+print(rotatedimage)
 
+pilimage = Image.fromarray(rotatedimage)
+pilimage.save("scatterRotate_image.png")
 
-max_frames = 90
-angle_animation = pi/180
-# Create n images of scatter and gather for ffmpeg video
-for i in range(0,max_frames):
-    rotatedimage = rotateScatter(image,angle_animation)
-    rotatedimage.shape = (-1,n)
+# print(rotatedimage)
 
-    plt.imshow(rotatedimage,cmap="gray")
-    plt.savefig("scatter/scatterRotate_image"+ str(i) + ".png")
+# plt.imshow(rotatedimage,cmap="gray")
+# plt.savefig("scatterRotate_image.png")
 
 
-    # inverseimage = rotateGather(image,angle_animation)
+# inverseimage = rotateGather(image,angle)
 
-    # inverseimage.shape = (-1,n)
-    # plt.imshow(inverseimage,cmap="gray")
-    # plt.savefig("gather/gatherRotate_image" + str(i) + ".png")
-    angle_animation+=pi/180
+# inverseimage.shape = (-1,n)
+# plt.imshow(inverseimage,cmap="gray")
+# plt.savefig("gatherRotate.png")
+
+
+# max_frames = 90
+# angle_animation = pi/180
+# # Create n images of scatter and gather for ffmpeg video
+# for i in range(0,max_frames):
+    
+#     rotatedimage = rotateScatter(image,angle_animation)
+    
+#     rotatedimage.shape = dims_tuple
+    
+#     plt.imshow(rotatedimage,cmap="gray")
+#     plt.savefig("scatter/scatterRotate_image"+ str(i) + ".png")
+
+
+#     # inverseimage = rotateGather(image,angle_animation)
+
+#     # inverseimage.shape = (-1,n)
+#     # plt.imshow(inverseimage,cmap="gray")
+#     # plt.savefig("gather/gatherRotate_image" + str(i) + ".png")
+#     angle_animation+=pi/180
 
 
