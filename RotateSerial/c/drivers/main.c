fac53ce1a14c889ca5154377e5656257deb2b3d9
Dylan Ramelli <rameld@usi.ch>
2023-02-21 12:03:24 +0100

    grazie Patrick, remember to initialize members of destination array before trying to access/assign data!

--- RotateSerial/c/drivers/main.c
+++ RotateSerial/c/drivers/main.c
@@ -13,26 +13,55 @@ void rotateScatter(const float A[], float dst_array[], const float angle,
   float c_y = height / 2.0;
   int size = width * height;
 
-  // Iterate over size of dst_array.
-  for (int i = 0; i < size; i++) {
+  // Double for loop i,j
+  // To check with debugger. Check if angle is 0 image should be the same.
+  // Iterate so that you know which indeces you are looking at.
 
-    float x = floor(i / height);
-    float y = floor(i % height);
+  for (int i = 0; i < height; i++) {
+    for (int j = 0; j < width; j++) {
 
-    x = x - c_x;
-    y = y - c_y;
+      float x = i - c_x;
+      float y = j - c_y;
 
-    float dst_x = cos(angle) * x - sin(angle) * y;
-    float dst_y = sin(angle) * x + cos(angle) * y;
+      // printf("x:%f\n", x);
+      // printf("y:%f\n", y);
 
-    dst_x = (int)dst_x + c_x;
-    dst_y = (int)dst_y + c_y;
+      float dst_x = cos(angle) * x - sin(angle) * y;
+      float dst_y = sin(angle) * x + cos(angle) * y;
 
-    if (dst_x >= 0 && dst_x < width && dst_y >= 0 && dst_y < height) {
-      int idx = dst_x * height + dst_y;
-      dst_array[idx] = A[i];
+      dst_x = (int)(dst_x + c_x);
+      dst_y = (int)(dst_y + c_y);
+
+      if (dst_x >= 0 && dst_x < width && dst_y >= 0 && dst_y < height) {
+        // printf("dstx:%f\n", dst_x);
+        // printf("dsty:%f\n", dst_y);
+        int idx = dst_y * width + dst_x;
+        // printf("%i,\n", idx);
+        dst_array[idx] = A[i * width + j];
+      }
     }
   }
+
+  // // Iterate over size of dst_array.
+  // for (int i = 0; i < size; i++) {
+
+  //   float x = floor(i / height);
+  //   float y = floor(i % height);
+
+  //   x = x - c_x;
+  //   y = y - c_y;
+
+  //   float dst_x = cos(angle) * x - sin(angle) * y;
+  //   float dst_y = sin(angle) * x + cos(angle) * y;
+
+  //   dst_x = (int)dst_x + c_x;
+  //   dst_y = (int)dst_y + c_y;
+
+  //   if (dst_x >= 0 && dst_x < width && dst_y >= 0 && dst_y < height) {
+  //     int idx = dst_x * height + dst_y;
+  //     dst_array[idx] = A[i];
+  //   }
+  // }
 }
 
 int main(int argc, char *argv[]) {
@@ -53,10 +82,20 @@ int main(int argc, char *argv[]) {
 
   for (int i = 0; i < n; ++i) {
     A[i] = A[i] / 255.0;
+    // if (i < width) {
+    //   A[i] = 0.0;
+    // }
   }
 
+  memset(result, 0, n * sizeof(float));
+
   // Rotate values of 1D array.
-  rotateScatter(A, result, M_PI / 16.0, height, width);
+  rotateScatter(A, result, M_PI / 4, width, height);
+
+  printf("#########################################");
+  for (int i = 0; i < n; i++) {
+    printf("%f\n", result[i]);
+  }
 
   FILE *fp = fopen("test_image.raw", "wb");
   if (fp) {
