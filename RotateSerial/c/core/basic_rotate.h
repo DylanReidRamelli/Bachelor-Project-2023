83cf45baecbe416703a3b6339c83adb4b4e7ea6a
Dylan Ramelli <rameld@usi.ch>
2023-02-21 14:57:14 +0100

    all rotations should work now, need to give python the new dimentions of the image modified in no loss rotation

--- RotateSerial/c/core/basic_rotate.h
+++ RotateSerial/c/core/basic_rotate.h
@@ -1,6 +1,86 @@
 #include <math.h>
 #include <stdio.h>
 
+// Find max value in array of float.
+float max(float input[], int size) {
+  float max_value = 0;
+  for (int i = 0; i < size; i++) {
+    if (input[i] > max_value)
+      max_value = input[i];
+  }
+  return max_value;
+}
+
+// Find min value in array of float.
+float min(float input[], int size) {
+  float min_value = 0;
+  for (int i = 0; i < size; i++) {
+    if (input[i] < min_value)
+      min_value = input[i];
+  }
+  return min_value;
+}
+
+// 2D rotation.
+void rotation(float x, float y, float angle) {
+  float tmp_x = x;
+  float tmp_y = y;
+  x = cos(angle) * tmp_x - sin(angle) * tmp_y;
+  y = sin(angle) * tmp_x + cos(angle) * tmp_y;
+}
+
+// 2D corner rotation.
+void rotateCorners(float output[2], int width, int height, float angle) {
+
+  float c_x = width / 2.0;
+  float c_y = height / 2.0;
+
+  // float ld[2] = {0, 0};
+  // float lu[2] = {0, height};
+  // float rd[2] = {width, 0};
+  // float ru[2] = {width, height};
+
+  float corners[8] = {0, 0, 0, height, width, 0, width, height};
+
+  for (int i = 0; i < 8; i = i + 2) {
+
+    for (int j = 0; j < 8; j++) {
+      if (j % 2 == 0) {
+        corners[j] = corners[j] - c_x;
+      } else {
+        corners[j] = corners[j] - c_y;
+      }
+    }
+
+    rotation(corners[i], corners[i + 1], angle);
+
+    for (int j = 0; j < 8; j++) {
+      if (j % 2 == 0) {
+        corners[j] = corners[j] + c_x;
+      } else {
+        corners[j] = corners[j] + c_y;
+      }
+    }
+  }
+
+  float x_values[] = {corners[0], corners[2], corners[4], corners[6]};
+  float y_values[] = {corners[1], corners[3], corners[5], corners[7]};
+  int sizeX = ceil(max(x_values, 4) - min(x_values, 4));
+  int sizeY = ceil(max(y_values, 4) - min(y_values, 4));
+
+  output[0] = sizeX;
+  output[1] = sizeY;
+}
+
+/**
+ * Rotate an image by iterating over the input array.
+ * @param A, input array of type float.
+ * @param dst_array, output array of type float.
+ * @param angle, angle we want to rotate by.
+ * @param width, width of the image.
+ * @param height, height of the image.
+ * @return void
+ **/
 void rotateScatter(const float A[], float dst_array[], const float angle,
                    const int width, const int height) {
 
@@ -8,23 +88,115 @@ void rotateScatter(const float A[], float dst_array[], const float angle,
   float c_y = height / 2.0;
   int size = width * height;
 
-  // Iterate over size of dst_array.
-  for (int i = 0; i < size; i++) {
-    float x = floor(i / height);
-    float y = floor(i % height);
+  // Iterating horizontally through the image.
+  for (int i = 0; i < height; i++) {
+    for (int j = 0; j < width; j++) {
 
-    x = x - c_x;
-    y = y - c_y;
+      // Subtract center coordinates, so that we rotate with respect to the
+      // center of the image.
+      float x = i - c_x;
+      float y = j - c_y;
 
-    float dst_x = cos(angle) * x - sin(angle) * y;
-    float dst_y = sin(angle) * x + cos(angle) * y;
+      // Rotation operation
+      float dst_x = cos(angle) * x - sin(angle) * y;
+      float dst_y = sin(angle) * x + cos(angle) * y;
 
-    dst_x = floor(dst_x + c_x);
-    dst_y = floor(dst_y + c_y);
+      // Add back the center "vector"
+      dst_x = (int)(dst_x + c_x);
+      dst_y = (int)(dst_y + c_y);
 
-    if (dst_x >= 0 && dst_x < width && dst_y >= 0 && dst_y < height) {
-      int idx = dst_x * height + dst_y;
-      dst_array[idx] = A[i];
+      // Check if the resulting point is inside the boundary of the image, i.e
+      // 0->max_x, 0->max_y.
+      if (dst_x >= 0 && dst_x < width && dst_y >= 0 && dst_y < height) {
+        // If so then assign value from original array to dst_array at idx
+        // location.
+        int idx = dst_y * width + dst_x;
+        dst_array[idx] = A[i * width + j];
+      }
+    }
+  }
+}
+
+/**
+ * Rotate an image by iterating over the output array.
+ * @param A, input array of type float.
+ * @param dst_array, output array of type float.
+ * @param angle, angle we want to rotate by.
+ * @param width, width of the image.
+ * @param height, height of the image.
+ * @return void
+ **/
+void rotateGather(const float A[], float dst_array[], const float angle,
+                  const int width, const int height) {
+  float c_x = width / 2.0;
+  float c_y = height / 2.0;
+  int size = width * height;
+
+  // Iterating horizontally through the image.
+  for (int i = 0; i < height; i++) {
+    for (int j = 0; j < width; j++) {
+
+      // Subtract center coordinates, so that we rotate with respect to the
+      // center of the image.
+      float x = i - c_x;
+      float y = j - c_y;
+
+      // Rotation operation
+      float dst_x = cos(angle) * x - sin(angle) * y;
+      float dst_y = sin(angle) * x + cos(angle) * y;
+
+      // Add back the center "vector"
+      dst_x = (int)(dst_x + c_x);
+      dst_y = (int)(dst_y + c_y);
+
+      // Check if the resulting point is inside the boundary of the image, i.e
+      // 0->max_x, 0->max_y.
+      if (dst_x >= 0 && dst_x < width && dst_y >= 0 && dst_y < height) {
+        // If so then assign value from original array to dst_array at idx
+        // location.
+        int idx = dst_y * width + dst_x;
+        dst_array[i * width + j] = A[idx];
+      }
+    }
+  }
+}
+
+void rotateGatherNoLoss(const float A[], float dst_array[], const float angle,
+                        const int width, const int height) {
+
+  float newSize[2] = {0.0, 0.0};
+  rotateCorners(newSize, width, height, angle);
+
+  float c_x = width / 2.0;
+  float c_y = height / 2.0;
+  float c_x_out = newSize[0] / 2.0;
+  float c_y_out = newSize[1] / 2.0;
+
+  // Iterating horizontally through the image.
+  for (int i = 0; i < height; i++) {
+    for (int j = 0; j < width; j++) {
+
+      // Subtract center coordinates, so that we rotate with respect to the
+      // center of the image.
+      float x = i - c_x_out;
+      float y = j - c_y_out;
+
+      // Rotation operation
+      float dst_x = cos(angle) * x - sin(angle) * y;
+      float dst_y = sin(angle) * x + cos(angle) * y;
+
+      // Add back the center "vector"
+      dst_x = (int)(dst_x + c_x);
+      dst_y = (int)(dst_y + c_y);
+
+      // Check if the resulting point is inside the boundary of the image,i.e
+      // 0->max_x, 0->max_y.
+      if (dst_x >= 0 && dst_x < width && dst_y >= 0 && dst_y < height) {
+        // If so then assign value from original array to dst_array at idx
+        // location.
+        int idx = dst_y * width + dst_x;
+        dst_array[i * width + j] = A[idx];
+      }
     }
   }
 }
--- RotateSerial/c/drivers/main.c
+++ RotateSerial/c/drivers/main.c
@@ -1,4 +1,4 @@
-// #include "../core/basic_rotate.h"
+#include "../core/basic_rotate.h"
 #include "../core/read-png.h"
 #include <assert.h>
 #include <math.h>
@@ -6,60 +6,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/**
- * Rotate an image by iterating over the input array.
- * @param A, input array of type float.
- * @param dst_array, output array of type float.
- * @param angle, angle we want to rotate by.
- * @param width, width of the image.
- * @param height, height of the image.
- * @return void
- **/
-void rotateScatter(const float A[], float dst_array[], const float angle,
-                   const int width, const int height) {
-
-  float c_x = width / 2.0;
-  float c_y = height / 2.0;
-  int size = width * height;
-
-  // Iterating horizontally through the image.
-  for (int i = 0; i < height; i++) {
-    for (int j = 0; j < width; j++) {
-
-      // Subtract center coordinates, so that we rotate with respect to the
-      // center of the image.
-      float x = i - c_x;
-      float y = j - c_y;
-
-      // Rotation operation
-      float dst_x = cos(angle) * x - sin(angle) * y;
-      float dst_y = sin(angle) * x + cos(angle) * y;
-
-      // Add back the center "vector"
-      dst_x = (int)(dst_x + c_x);
-      dst_y = (int)(dst_y + c_y);
-
-      // Check if the resulting point is inside the boundary of the image, i.e
-      // 0->max_x, 0->max_y.
-      if (dst_x >= 0 && dst_x < width && dst_y >= 0 && dst_y < height) {
-        // If so then assign value from original array to dst_array at idx
-        // location.
-        int idx = dst_y * width + dst_x;
-        dst_array[idx] = A[i * width + j];
-      }
-    }
-  }
-}
-
 int main(int argc, char *argv[]) {
   // const char *pathname = "../../images/Roberts-Claude-Shannon-1.png";
   const char *pathname = "../../images/data_rectangle.raw";
   int width = 300;
   int height = 200;
 
-  if (argv == 3) {
-    width = argv[1];
-    height = argv[2];
+  if (argc == 3) {
+    width = atoi(argv[1]);
+    height = atoi(argv[2]);
   }
 
   // Declare initial variables.
@@ -82,7 +37,9 @@ int main(int argc, char *argv[]) {
   }
 
   // Rotate values of 1D input array and store in result.
-  rotateScatter(A, result, M_PI / 4, width, height);
+  // rotateScatter(A, result, M_PI / 4, width, height);
+  // rotateGather(A, result, M_PI / 4, width, height);
+  rotateGatherNoLoss(A, result, M_PI / 4, width, height);
 
   // Open output file and write result array.
   FILE *fp = fopen("test_image.raw", "wb");
