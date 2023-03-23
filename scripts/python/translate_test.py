f52c5960f4b44e7321a3cf0a37bd3e87459a40ad
Dylan Ramelli <rameld@usi.ch>
2023-03-23 17:01:00 +0100

    ...

--- scripts/python/translate_test.py
+++ scripts/python/translate_test.py
@@ -14,17 +14,7 @@ print(X)
 
 
 
-# Create phase shift vector
-# N = len(x)
-# f = np.arange(N) / N
-# phase_shift = 2 * np.pi * 13.33333 * f
-# exp_phase_shift = np.exp(-1j * phase_shift)
 
-# # Apply phase shift in frequency domain
-# X_shifted = X * exp_phase_shift
-
-# # Perform inverse FFT
-# x_shifted = np.fft.ifft(X_shifted)
 
 # # Display original and shifted signals
 # print('Original signal:')
